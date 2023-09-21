3ce1a1d14aaea2ecf45972ec33c4176e68e65221
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-09-21 07:56:47 -0700

    doc: Header file

---
+++ myshell.h
@@ -0,0 +1,27 @@
+#ifndef MYSHELL_HEADER
+#define MYSHELL_HEADER
+
+/*---> HEADER FILES <---*/
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/*---> CONSTANTS <---*/
+
+/*---> FUNCTION PROTOTYPES <---*/
+size_t my_list_len(const list_t *head);
+
+
+/* string.h implemetation */
+
+
+#endif /*MYSHELL_HEADER*/
